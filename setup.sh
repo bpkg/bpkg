509063ee932b6d3a9670c4ab01c392e20971268f
jwerle <joseph.werle@gmail.com>
2022-03-30 18:53:29 -0400

    chore(): 1.0.20

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@
 #        "               ""
 #        bash package manager
 
-VERSION="1.0.19"
+VERSION="1.0.20"
 TAG=${TAG:-$VERSION}
 BRANCH=${BRANCH:-$TAG}
 REMOTE=${REMOTE:-https://github.com/bpkg/bpkg.git}
