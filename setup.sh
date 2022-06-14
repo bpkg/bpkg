ea415ea0dda40bcafb84e8953f8efdf1a6bbb52b
Joseph Werle <joseph.werle@gmail.com>
2022-06-14 11:36:12 +0200

    chore(): 1.1.3

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@
 #        "               ""
 #        bash package manager
 
-VERSION="1.1.2"
+VERSION="1.1.3"
 TAG=${TAG:-$VERSION}
 BRANCH=${BRANCH:-$TAG}
 REMOTE=${REMOTE:-https://github.com/bpkg/bpkg.git}
